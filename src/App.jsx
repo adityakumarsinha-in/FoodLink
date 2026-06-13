c9735d484715913412eed28325c5ec71cbf5c135
Aditya Kumar Sinha <94775186+adityakumarsinha-in@users.noreply.github.com>
2026-06-13 14:16:01 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AuthProvider>
         <FoodProvider>
           <AppRoutes />
